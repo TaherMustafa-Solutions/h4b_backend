11c3b5d5167179ca1c7732eab7c588218eca0acf
saroyar <yarsaro2001@gmail.com>
2023-07-09 20:54:33 +0530

    get user function working

--- controller/userController.js
+++ controller/userController.js
@@ -1,88 +1,103 @@
-const User=require("../models/userModel")
-const cloudinary=require('cloudinary').v2
-const bcrypt=require('bcryptjs')
-exports.createUser=async(req,res)=>{
-
-    try {
-        const {name,email,password,address,phone_no}=req.body;
-         let result
-         console.log(req.files)
-         if(!req.files){
-            res.status(400).json({
-                error:"Profile photo Required",
-            })
-         }
-        if(!name||!email||!password||!phone_no)
-        {
-            res.status(400).json({
-                success:false,
-                error:"require all element"
-            })
+const User = require("../models/userModel");
+const cloudinary = require("cloudinary").v2;
+const bcrypt = require("bcryptjs");
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email, password, address, phone_no } = req.body;
+    let result;
+    console.log(req.files);
+    if (!req.files) {
+      res.status(400).json({
+        error: "Profile photo Required",
+      });
+    }
+    if (!name || !email || !password || !phone_no) {
+      res.status(400).json({
+        success: false,
+        error: "require all element",
+      });
+    }
+    const file = req.files.profile;
+    result = await cloudinary.uploader.upload(file.tempFilePath, {
+      folder: "users",
+      width: 150,
+      crop: "scale",
+    });
+    const user = await User.create({
+      name,
+      email,
+      password,
+      phone_no,
+      address,
+      photo: {
+        id: result.public_id,
+        secure_url: result.secure_url,
+      },
+    });
+    user.password = undefined;
+    res.status(201).json({
+      result: true,
+      id: user._id,
+    });
+  } catch (error) {
+    res.status(201).json({
+      success: false,
+      error,
+    });
+  }
+};
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      res.json({
+        result: false,
+      });
+    } else {
+      User.find({ email }).then(async (data) => {
+        if (data.length === 0) {
+          res.json({
+            result: false,
+          });
+        } else {
+          const result = await bcrypt.compare(password, data[0].password);
+          if (result == true) {
+            res.json({
+              result: true,
+              id: data[0]._id,
+            });
+          } else {
+            res.json({
+              result: false,
+            });
+          }
         }
-        const file=req.files.profile
-        result=await cloudinary.uploader.upload(file.tempFilePath,{
-            folder:'users',
-            width:150,
-            crop:"scale"
-        })
-        const user=await User.create({
-            name,
-            email,
-            password,
-            phone_no,
-            address,
-            photo:{
-                id:result.public_id,
-                secure_url:result.secure_url
-            }
-        })
-        user.password=undefined
-        res.status(201).json({
-            result:true,
-            id:user._id
-        })
-    } catch (error) {
-        res.status(201).json({
-            success:false,
-            error
-        })
+      });
     }
-}
-exports.loginUser=async(req,res)=>{
-     const {email,password}=req.body
-     try{
-     if(!email || !password){
-        res.json({
-            result:false
-        })
-     }
-     else{
-        User.find({email}).then(async(data)=>{
-            if(data.length===0){
-                res.json({
-                    result:false
-                })
-            }
-            else{
-                const result=await bcrypt.compare(password,data[0].password)
-                if(result==true){
-                    res.json({
-                        result:true,
-                        id:data[0]._id
-                    })
-                }
-                else{
-                    res.json({
-                        result:false
-                    })
-                }
-            }
-        })
-     }
+  } catch (err) {
+    res.josn({
+      result: "Server error",
+    });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      res.json({
+        result: false,
+        message: "userId not found",
+      });
     }
-    catch(err){
-      res.josn({
-        result:"Server error"
-      })
-    }
-}
+    res.json({
+      result: true,
+      user,
+    });
+  } catch (error) {
+    res.json({
+      result: "server error",
+    });
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,8 @@
 const express=require("express");
-const { createUser,loginUser } = require("../controller/userController");
+const { createUser,loginUser, getUser } = require("../controller/userController");
 const router=express.Router();
 
 router.post("/signup",createUser);
-router.post("/login",loginUser)
+router.post("/login",loginUser);
+router.get("/getUser/:id",getUser)
 module.exports=router
