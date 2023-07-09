5554c2ecd7b98845fde0c4372a869d6f3d59892d
TaherMustafa-Solutions <tmsolutions5372@gmail.com>
2023-07-09 12:25:06 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express=require('express')
+const bodyParser=require('body-parser')
+const cors=require('cors')
+const mongoose=require('mongoose')
+const fileUpload=require("express-fileupload")
+require('dotenv').config()
+const port=process.env.PORT
+const app=express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+
+app.listen(port,()=>{
+    console.log(`connected at ${port}`)
+})
+
+
