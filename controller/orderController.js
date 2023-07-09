9f39864975027a909293c7d72d004fbc0dc52673
TaherMustafa-Solutions <tmsolutions5372@gmail.com>
2023-07-10 04:33:02 +0530

    Order model updated finally we have done

--- controller/orderController.js
+++ controller/orderController.js
@@ -2,61 +2,58 @@ const Order=require('../models/orderModel')
 const User=require('../models/userModel')
 exports.order=async(req,res)=>{
     const userId=req.params.id
-    res.json({
-        id:userId
-    })
-    // let {newspaper,cardboard,aluminum,iron,paper,book,magazine,plastic}=req.body
-    //  newspaper=newspaper?+newspaper:0
-    //  cardboard=cardboard?+cardboard:0
-    //  aluminum=aluminum?+aluminum:0
-    //  iron=iron?+iron:0
-    //  paper=paper?+paper:0
-    //  book=book?+book:0
-    //  magazine=magazine?+magazine:0
-    //  plastic=plastic?plastic:0
-    //  const order=await Order.create({
-    //     newspaper:{
-    //         weight:newspaper,
-    //         rate:14*newspaper
-    //     },
-    //     cardboard:{
-    //         weight:cardboard,
-    //         rate:5*cardboard
-    //     },
-    //     aluminum:{
-    //         weight:aluminum,
-    //         rate:105*aluminum
-    //     },
-    //     iron:{
-    //         weight:iron,
-    //         rate:iron*26
-    //     },
-    //     paper:{
-    //         weight:paper,
-    //         rate:15*paper
-    //     },
-    //     book:{
-    //         weight:book,
-    //         rate:12*book
-    //     },
-    //     magazine:{
-    //         weight:magazine,
-    //         rate:15*magazine
-    //     },
-    //     plastic:{
-    //         weight:plastic,
-    //         rate:10*plastic
-    //     },
-    //     userId,
-    //     totalQuantity:newspaper+cardboard+aluminum+iron+paper+book+magazine+plastic
-    //  })
-    //  Order.find({_id:order._id}).populate('userId').then((data)=>{
+    let {newspaper,cardboard,aluminum,iron,paper,book,magazine,plastic}=req.body
+     newspaper=newspaper?+newspaper:0
+     cardboard=cardboard?+cardboard:0
+     aluminum=aluminum?+aluminum:0
+     iron=iron?+iron:0
+     paper=paper?+paper:0
+     book=book?+book:0
+     magazine=magazine?+magazine:0
+     plastic=plastic?plastic:0
+     const order=await Order.create({
+        newspaper:{
+            weight:newspaper,
+            rate:14*newspaper
+        },
+        cardboard:{
+            weight:cardboard,
+            rate:5*cardboard
+        },
+        aluminum:{
+            weight:aluminum,
+            rate:105*aluminum
+        },
+        iron:{
+            weight:iron,
+            rate:iron*26
+        },
+        paper:{
+            weight:paper,
+            rate:15*paper
+        },
+        book:{
+            weight:book,
+            rate:12*book
+        },
+        magazine:{
+            weight:magazine,
+            rate:15*magazine
+        },
+        plastic:{
+            weight:plastic,
+            rate:10*plastic
+        },
+        userId,
+        totalQuantity:newspaper+cardboard+aluminum+iron+paper+book+magazine+plastic
+     })
+     Order.find({_id:order._id}).populate('userId').then((data)=>{
         
-    //     res.json({
-    //         result:true,
-    //         data
-    //     })
-    //  })
+        res.json({
+            result:true,
+            data
+        })
+     })
  
 }
 exports.orderPending=(req,res)=>{
