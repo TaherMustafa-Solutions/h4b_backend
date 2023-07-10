fe8ff3e5de22759177e930f19e8eb0191249d53e
TaherMustafa-Solutions <tmsolutions5372@gmail.com>
2023-07-10 08:58:49 +0530

    Signup route updated

--- controller/userController.js
+++ controller/userController.js
@@ -6,12 +6,6 @@ exports.createUser=async(req,res)=>{
     try {
         const {name,email,password,address,phone_no}=req.body;
          let result
-         console.log(req.files)
-         if(!req.files){
-            res.status(400).json({
-                error:"Profile photo Required",
-            })
-         }
         if(!name||!email||!password||!phone_no)
         {
             res.status(400).json({
@@ -19,32 +13,22 @@ exports.createUser=async(req,res)=>{
                 error:"require all element"
             })
         }
-        const file=req.photo
-        result=await cloudinary.uploader.upload(file.tempFilePath,{
-            folder:'users',
-            width:150,
-            crop:"scale"
-        })
         const user=await User.create({
             name,
             email,
             password,
             phone_no,
             address,
-            photo:{
-                id:result.public_id,
-                secure_url:result.secure_url
-            }
+           
         })
         user.password=undefined
         res.status(201).json({
             result:true,
             id:user._id,
-            file
         })
     } catch (error) {
         res.status(201).json({
-            success:false,
+            result:false,
             error
         })
     }
